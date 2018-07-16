19dbe75f9d0b20731625b7354b6a65c3a72ff03f
Jack Laird <jack.laird0@gmail.com>
2018-07-16 09:49:05 -0600

    Add schema for items in migrations

--- db/migrations/20180716092731_initial.js
+++ db/migrations/20180716092731_initial.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('items', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.boolean('status');
+
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.droptable('items')
+  ]);
 };
