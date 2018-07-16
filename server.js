58d60008f2dcaa538ba26d0966a1d9f78f585e5b
Jack Laird <jack.laird0@gmail.com>
2018-07-16 10:05:00 -0600

    Add static page for server to use

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
-
 
 
 app.listen(app.get('port'), () => {
