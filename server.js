ea7492cc63d4c660a76a6237426667a9da789814
Jack Laird <jack.laird0@gmail.com>
2018-07-16 11:17:59 -0600

    Create post request for items endpoint

--- server.js
+++ server.js
@@ -20,6 +20,25 @@ app.get('/api/v1/items', (req, resp) => {
     })
 });
 
+app.post('/api/v1/items', (req, resp) => {
+  const { item  } = req.body;
+
+  for(let requiredParam of ['name', 'status']) {
+    if(!item[requiredParam]) {
+      return resp.status(422)
+        .send({ error: `Expected format: { item: { name: <String>, status: <Boolean> }} You\'re missing a ${requiredParam} property.`})
+    }
+  }
+
+  database('items').insert(item, 'id')
+    .then(item => {
+      resp.status(201).json({ id: item[0]})
+    })
+    .catch(error => {
+      resp.status(500).json({ error })
+    })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`Mars Bars is running on ${app.get('port')}`)
 })
