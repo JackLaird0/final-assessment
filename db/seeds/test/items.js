7e42e34e755b4d0f61a0d95afd874d6e5a931d9a
Jack Laird <jack.laird0@gmail.com>
2018-07-16 11:59:37 -0600

    Add another item to test seeding

--- db/seeds/test/items.js
+++ db/seeds/test/items.js
@@ -2,7 +2,8 @@ exports.seed = function(knex, Promise) {
   return knex('items').del()
     .then(function () {
       return Promise.all([
-        knex('items').insert({name: 'item 1', status: false})
+        knex('items').insert({name: 'item 1', status: false}),
+        knex('items').insert({name: 'item 2', status: false})
       ])
       .then(() => console.log('Seeding Complete!'))
     })
