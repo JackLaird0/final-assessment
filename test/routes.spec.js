458b7aaedf27cafa4aadeb6822ed3fd268a2e6f3
Jack Laird <jack.laird0@gmail.com>
2018-07-16 11:43:04 -0600

    Test that server returns homepage at route endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -9,7 +9,15 @@ const knex = require('knex')(configuration);
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  
+  it('should return the homepage when hitting the root endpoint', done => {
+    chai.request(server)
+      .get('/')
+      .end((err, resp) => {
+        resp.should.have.status(200);
+        resp.should.be.html;
+        done();
+      })
+  })
 })
 
 describe('API Routes', () => {
