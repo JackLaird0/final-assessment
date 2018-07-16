cc19bc5fb223b031bfe50a13c7ef5861aafaac2f
Jack Laird <jack.laird0@gmail.com>
2018-07-16 11:10:01 -0600

    Create GET endpoint for items

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ const database = require('knex')(configuration);
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3050);
 
+app.get('/api/v1/items', (req, resp) => {
+  database('items').select()
+    .then( items => {
+      resp.status(200).json(items)
+    })
+    .catch( error => {
+      resp.status(500).json({ error })
+    })
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Mars Bars is running on ${app.get('port')}`)
